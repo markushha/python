a3033b2edd12487f69deef0b13d97c6fbe69e3ce
Mark <ingerkrut@gmail.com>
2023-12-20 22:41:49 +0600

    learned string format() function;

---
+++ essentials/str_format.py
@@ -0,0 +1,9 @@
+# str.format()
+
+print("Another way of {}, quite weird {}".format("interpolation", "tho"))
+
+num = 3.123456789
+sum = 34599
+
+print("A number is {:.2f}".format(num))
+print("Your total is {:,} KZT".format(sum))
